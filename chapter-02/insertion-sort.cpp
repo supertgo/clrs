3b06b6ff3a8b3289379578faa4d2d11134ae95c3
supertgo <thiagoroberto2003@gmail.com>
2023-01-08 07:21:59 -0300

    feat(sorting): add insertion sort decreasing

--- chapter-02/insertion-sort.cpp
+++ chapter-02/insertion-sort.cpp
@@ -14,16 +14,52 @@ void insertionSort(T data[], int n) {
   }
 }
 
+template <typename T>
+void insertionSortDecreasing(T data[], int n) {
+  for (int i = 0, j; i < n; i++) {
+    T tmp = data[i];
 
-int main () {
-    int v[] = {2, 3, 1, 6, 5, 4};
-    double u[] = {1.4, 2.2, 3.1, 4, 5.6, 6.8, 7, 8.1, 9.3, 10};
-
-    insertionSort(u, 10);
-
-    for (int i = 0; i < 10; i++) {
-      cout << u[i] << ' ';
+    for (j = i; j > 0 && tmp > data[j - 1]; j--) {
+      data[j] = data[j - 1];
     }
 
-    return 0;
+    data[j] = tmp;
+  }
+}
+
+template <typename T>
+int linear_search(T data[], T x, int n) {
+  for (int i = 0; i < n; i++) {
+    if (data[i] == x) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+int* add_binary_intergers(int a[], int b[], int n) {
+  int *c = new int[n + 1];
+  int carry = 0;
+
+  for (int i = 0; i < n; i++) {
+    c[i] = (a[i] + b[i] + carry) % 2;
+    carry = (a[i] + b[i] + carry) / 2;
+  }
+
+  c[n + 1] = carry;
+
+  return c;
+}
+
+int main () {
+  int v[] = {1, 0, 0};
+  int u[] = {1, 0, 0};
+  double w[] = {1.4, 2.2, 3.1, 4, 5.6, 6.8, 7, 8.1, 9.3, 10};
+
+  int *newArray = add_binary_intergers(v, u, 3);
+  
+
+  return 0;
 }
