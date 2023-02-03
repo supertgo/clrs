25c7ab5d66f855f4d8aa36e643939633f0c382e9
supertgo <thiagoroberto2003@gmail.com>
2023-02-03 14:29:01 -0300

    feat: add method to check if the matrices can be multiplied

--- divide_and_coquer/matrices.cpp
+++ divide_and_coquer/matrices.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int get_matrix_column(float** M) {
+  return sizeof(M) / sizeof(M[0][0]);
+}
+
+int get_matrix_row(float**M ) {
+  return sizeof(M) / sizeof(M[0]);
+}
+
+bool check_if_its_possible_to_multiplication(float** A, float** B) {
+  if(get_matrix_column(A) == get_matrix_row(B)) {
+    return 1;
+  }
+
+  return 0;
+}
+
 void matrices_multiplication(float** A, float** B, float** P, int size) {
+  if(check_if_its_possible_to_multiplication(A, B)) return;
+
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) { 
       for (int k = 0; k < size; k++) {
