f0878ed3b43a3acbaaa3a6281e256cdcf3a5fb57
supertgo <thiagoroberto2003@gmail.com>
2023-02-20 17:02:36 -0300

    feat(trees): add morris inorder algo

--- trees/genBST.h
+++ trees/genBST.h
@@ -197,3 +197,33 @@ void BST<T>::iterativeInorder() {
     else p = 0;
   }
 }
+
+template<class T>
+void BST<T>::MorrisInorder() {
+  BSTNode<T> *p = root, *tmp;
+
+  while (p != 0)
+    if (p->left == 0) {
+      visit(p);
+      p = p->right;
+    }
+    else {
+      tmp = p->left;
+      while (
+        tmp->right != 0 &&
+        tmp->right != p 
+      ) {
+        tmp = tmp->right;
+      }
+
+      if (tmp->right == 0) {
+        tmp->right = p;
+        p = p->left;
+      }
+      else {
+        visit(p);
+        tmp->right = 0;
+        p = p->right;
+      }
+    }
+}
